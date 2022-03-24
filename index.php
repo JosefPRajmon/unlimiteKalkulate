3e3cd22afc81203040fe518c214ad9aa7cb0864c
Pepa rajmon <rajmon.josef1999@seznam.cz>
2022-03-24 09:57:11 +0100

    upgrade inputs

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
+<?php
+if (isset($_POST["submit"])){
+    $firstNumber = $_POST["firstNumber"];
+    $secondNumber = $_POST["secontNumber"];
+}
+
+
+?>
+
 <form method="post">
     <label>
-        <input type="number" name="firstNumber" placeholder="první číslo">
+        <input type="number" name="firstNumber" placeholder="první číslo" value=<?php echo $firstNumber ?> >
     </label>
     <br>
     <label>
-        <input type="number" name="secontNumber" placeholder="druhé cislo">
+        <input type="number" name="secontNumber" placeholder="druhé cislo" value=<?php echo $secondNumber ?> >
     </label>
     <br>
     <input type="submit" name="submit">
 </form>
 
 <?php
- if (isset($_POST["submit"])){
-     $firstNumber = $_POST["firstNumber"];
-     $secondNumber = $_POST["secontNumber"];
-
-     echo bcadd($firstNumber, $secondNumber);
- }
-
-
-?>
+if (isset($_POST["submit"])){
+    echo bcadd($firstNumber, $secondNumber);
+}
