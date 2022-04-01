49e56a991bdf13adec4f161d193c0d56064ffac1
Josef Rajmon <rajmon.josef1999@seznam.cz>
2022-04-01 11:58:03 +0200

    remove bcadd()

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_POST["submit"])){
-    $firstNumber = $_POST["firstNumber"];
-    $secondNumber = $_POST["secontNumber"];
+    $firstNumber = (string) $_POST["firstNumber"];
+    $secondNumber = (string) $_POST["secontNumber"];
 }
 
 
@@ -10,9 +10,7 @@ if (isset($_POST["submit"])){
 <form method="post">
     <label>
         <input type="number" name="firstNumber" placeholder="první číslo" value=<?php if (isset($_POST["submit"])){echo $firstNumber;} ?> >
-    </label>
-    <br>
-    <label>
+    +
         <input type="number" name="secontNumber" placeholder="druhé cislo" value=<?php if (isset($_POST["submit"])){ echo $secondNumber;} ?> >
     </label>
     <br>
@@ -21,5 +19,49 @@ if (isset($_POST["submit"])){
 
 <?php
 if (isset($_POST["submit"])){
-    echo bcadd($firstNumber, $secondNumber);
+    $arrayFirsNumber =str_split(strrev($firstNumber));
+    $arraySecondNumber = str_split(strrev($secondNumber));
+    $numerickRest = 0;
+    $number = 0;
+    if (count($arrayFirsNumber) > count($arraySecondNumber)){
+        $number = count($arrayFirsNumber);
+    }
+    else{
+        $number = count($arraySecondNumber);
+    }
+
+    $arraySumary=[];
+    for ($i = 0; $i < $number ; $i++){
+        $val1 = (int)($arrayFirsNumber[$i] ?? 0);
+        $val2 = (int)($arraySecondNumber[$i] ?? 0);
+        $sumary = $val1 + $val2 + $numerickRest;
+
+
+        if($sumary >= 20){
+            $sumary = $sumary -20;
+            $numerickRest = 2;
+            $arraySumary[$i]= $sumary;
+
+        }
+        elseif ($sumary >= 10){
+
+            $sumary = $sumary -10;
+            $numerickRest = 1;
+            $arraySumary[$i]= $sumary;
+
+        }
+        else{
+            $numerickRest = 0;
+            $arraySumary[$i]= $sumary;
+        }
+    }
+
+    $finalNumber ="";
+    for ($y = 0; $y<$number;$y++){
+        $finalNumber = $finalNumber . $arraySumary[$y];
+    }
+    $finalNumber = strrev($finalNumber);
+    echo $finalNumber;
+
+
 }
